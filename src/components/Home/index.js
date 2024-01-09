b5210175af66c9220b41e2f092350bf0a1e0b88a
shirisha-sheddy <shirishasheddy963@gmail.com>
2024-01-09 16:20:32 +0000

    IPL-Dashboard

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,69 +1,62 @@
+/* eslint-disable react/no-unknown-property */
 // Write your code here
-import {Component} from 'react'
+import './index.css'
 import Loader from 'react-loader-spinner'
-
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+import {Component} from 'react'
 import TeamCard from '../TeamCard'
 
-import './index.css'
-
-const teamsApiUrl = 'https://apis.ccbp.in/ipl'
-
 class Home extends Component {
+  /** the state method saves data obtained from url https://apis.ccbp.in/ipl */
   state = {
-    isLoading: true,
     teamsData: [],
+    isLoading: true,
   }
 
   componentDidMount() {
-    this.getTeams()
+    this.getTeamsList()
   }
 
-  getTeams = async () => {
-    const response = await fetch(teamsApiUrl)
-    const fetchedData = await response.json()
-    const formattedData = fetchedData.teams.map(team => ({
-      name: team.name,
-      id: team.id,
-      teamImageURL: team.team_image_url,
+  getTeamsList = async () => {
+    const response = await fetch('https://apis.ccbp.in/ipl')
+    const fetchData = await response.json()
+    const updatedData = fetchData.teams.map(eachData => ({
+      name: eachData.name,
+      imageUrl: eachData.team_image_url,
+      id: eachData.id,
     }))
-
-    this.setState({
-      teamsData: formattedData,
-      isLoading: false,
-    })
+    this.setState({teamsData: updatedData, isLoading: false})
   }
 
   renderTeamsList = () => {
     const {teamsData} = this.state
-
     return (
-      <ul className="teams-list">
+      <ul className="team-list-items">
         {teamsData.map(team => (
-          <TeamCard teamDetails={team} key={team.id} />
+          <TeamCard key={team.id} teamData={team} />
         ))}
       </ul>
     )
   }
 
   renderLoader = () => (
-    <div className="loader-container" testid="loader">
-      <Loader type="Oval" color="#ffffff" height={50} />
+    <div testid="loader" className="loader-container">
+      <Loader type="Rings" color="#00BFFF" height={80} width={80} />
     </div>
   )
 
   render() {
     const {isLoading} = this.state
-
     return (
-      <div className="home-route-container">
-        <div className="teams-list-container">
-          <div className="ipl-dashboard-heading-container">
+      <div className="app-container">
+        <div className="ipl-container">
+          <div className="header-container">
             <img
+              className="ipl-logo"
               src="https://assets.ccbp.in/frontend/react-js/ipl-logo-img.png"
               alt="ipl logo"
-              className="ipl-logo"
             />
-            <h1 className="ipl-dashboard-heading">IPL Dashboard</h1>
+            <h1 className="header-heading">IPL Dashboard</h1>
           </div>
           {isLoading ? this.renderLoader() : this.renderTeamsList()}
         </div>
--- src/components/TeamCard/index.js
+++ src/components/TeamCard/index.js
@@ -1,19 +1,21 @@
 // Write your code here
-import {Link} from 'react-router-dom'
 import './index.css'
+import {Link} from 'react-router-dom'
+import {Component} from 'react'
 
-const TeamCard = props => {
-  const {teamDetails} = props
-  const {name, id, teamImageURL} = teamDetails
-
-  return (
-    <li className="team-item">
+class TeamCard extends Component {
+  render() {
+    const {teamData} = this.props
+    const {name, imageUrl, id} = teamData
+    return (
       <Link to={`/team-matches/${id}`} className="link-item">
-        <img src={teamImageURL} alt={name} className="team-logo" />
-        <p className="team-name">{name}</p>
+        <li className="team-card">
+          <img className="team-card-image" src={imageUrl} alt={`${name}`} />
+          <p className="team-card-name">{name}</p>
+        </li>
       </Link>
-    </li>
-  )
+    )
+  }
 }
 
 export default TeamCard
